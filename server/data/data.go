a1d8ecb6e25a5109762db898330b27b37144dc85
Joaquim Castilla <xcastilla89@gmail.com>
2020-03-08 21:11:49 +0900

    Reorganized code and testing http request handler

--- server/data/data.go
+++ server/data/data.go
@@ -4,12 +4,16 @@ import (
 	"context"
 	"fmt"
 	"os"
-
+	"../models"
+	"time"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
-func GetDBConnection() (*mongo.Database, error) {
+var db *mongo.Database
+
+func InitDBConnection() (error) {
 	URI := fmt.Sprintf("mongodb://%s", os.Getenv("MONGO_URL"))
 	clientOptions := options.Client().ApplyURI(URI).
 		SetAuth(options.Credential{
@@ -19,14 +23,47 @@ func GetDBConnection() (*mongo.Database, error) {
 		})
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	err = client.Ping(context.TODO(), nil)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	db := client.Database(os.Getenv("MONGO_DB_NAME"))
-	return db, nil
+	db = client.Database(os.Getenv("MONGO_DB_NAME"))
+	return nil
+}
+
+
+// Return all readings starting from fromDate
+func GetReadings(fromDate time.Time) ([]models.SensorReading, error) {
+	collection := db.Collection("measurements2")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	
+	projection := bson.D{
+		{"_id", 0},
+		{"temperature", 1},
+		{"timestamp", 1},
+	}
+
+	var query = bson.M{"timestamp": bson.M{"$gt": fromDate}}
+	cur, err := collection.Find(ctx, query, options.Find().SetProjection(projection),)
+	if err != nil { 
+		return nil, err
+	}
+	defer cur.Close(ctx)
+
+	// Pack results
+	results := []models.SensorReading{}
+	for cur.Next(ctx) {
+		reading := models.SensorReading{}
+		err = cur.Decode(&reading)
+		if err != nil {
+			return nil, err
+		}
+		results = append(results, reading)
+	}
+
+	return results, nil
 }
--- server/main.go
+++ server/main.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"fmt"
-	"./models"
 	"./data"
+	"net/http"
+	"log"
 	"time"
 )
 
-func main() {
-	db, err := data.GetDBConnection()
-	fmt.Println(db, err)
-	fromDate := time.Date(2014, time.November, 4, 0, 0, 0, 0, time.UTC)
-	results, err := models.GetReadings(db, fromDate)
-	for _, d := range results {
-		fmt.Println(d)
+func handler(w http.ResponseWriter, r *http.Request) {
+	fromDate := time.Date(2019, time.November, 4, 0, 0, 0, 0, time.UTC)
+	results, err := data.GetReadings(fromDate)
+	if err != nil {
+		log.Fatal("Could not fetch readings")
 	}
+	for _, data := range results {
+		fmt.Fprintf(w, "%s %f\n", data.Timestamp, data.Temperature)
+	}
+}
 
+func main() {
+	err := data.InitDBConnection()
+	if err != nil {
+		log.Fatal("Couldn't initialize DB connection")
+		return
+	}
+	
+	http.HandleFunc("/", handler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
